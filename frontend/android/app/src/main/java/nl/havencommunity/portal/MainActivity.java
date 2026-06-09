a5d2e4f7f7cdfebcd5c2da71740641c4ac252915
Tim Kolijn <tim@kolijn.org>
2026-06-10 01:25:23 +0200

    add capacitor

---
+++ frontend/android/app/src/main/java/nl/havencommunity/portal/MainActivity.java
@@ -0,0 +1,5 @@
+package nl.havencommunity.portal;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nl.havencommunity.portal',
+  appName: 'Portal',
+  webDir: 'dist'
+};
+
+export default config;
